be4a6c0f94b479610a7fe54ec5d0a7c2203427bb
sharelain <126391452+sharelain@users.noreply.github.com>
2023-07-24 23:29:52 +0800

    Update site.config.js

--- config/site.config.js
+++ config/site.config.js
@@ -20,7 +20,7 @@ module.exports = {
   title: "ShareDrive",
 
   // The folder that you are to share publicly with onedrive-vercel-index. Use '/' if you want to share your root folder.
-  baseDirectory: '/',
+  baseDirectory: '/share/',
 
   // [OPTIONAL] This represents the maximum number of items that one directory lists, pagination supported.
   // Do note that this is limited up to 200 items by the upstream OneDrive API.
@@ -42,7 +42,7 @@ module.exports = {
 
   // [OPTIONAL] This is where you specify the folders that are password protected. It is an array of paths pointing to all
   // the directories in which you have .password set. Check the documentation for details.
-  protectedRoutes: ['/🔒 Locked'],
+  protectedRoutes: ['/根目录/share/🔒 Locked'],
 
   // [OPTIONAL] Use "" here if you want to remove this email address from the nav bar.
   email: 'xiongjum@outlook.com',
